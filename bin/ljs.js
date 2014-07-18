f858fba06072b980808d3b7b52051ef3c89b7a89
Oleg Grenrus <oleg.grenrus@iki.fi>
2014-07-18 14:14:18 +0300

    Use commander

--- bin/ljs.js
+++ bin/ljs.js
@@ -34,71 +34,55 @@
 
 "use strict";
 
-var optimist = require("optimist");
+var program = require("commander");
 var fs = require("fs");
+var path = require("path");
 
 var literate = require("../lib/literate.js");
 
-optimist.usage("ljs [options] file.js");
+var pkgJson = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "package.json")).toString());
 
-optimist.boolean("h").options("h", {
-  alias: "help",
-  describe: "Show brief help information",
-});
+function toBool(x) {
+  if (x === "true") {
+    return true;
+  } else if (x === "false") {
+    return false;
+  } else {
+    return true;
+  }
+}
 
-optimist.boolean("v").options("v", {
-  alias: "version",
-  describe: "Display version information and exit.",
-});
-
-optimist.options("o", {
-  alias: "output",
-  describe: "Output file.",
-});
-
-optimist.boolean("c").options("c", {
-  alias: "code",
-  describe: "Include code in output file.",
-  default: true,
-});
+program.usage("[options] file.js");
+program.version(pkgJson.version);
+program.option("-o, --output <file>", "Output file");
+program.option("-c, --code <switch>", "Include code in output file", toBool);
 
 function cli(argv) {
-  var options = optimist.parse(argv);
+  program.parse(argv);
 
-  if (options.help) {
-    console.log(optimist.help());
-    return 0;
-  }
-
-  if (options.version) {
-    var pkg = JSON.parse(fs.readFileSync(__dirname + "/../package.json"));
-    console.log("jsgrep, part of jsstana version " + pkg.version);
-    return 0;
-  }
-
-  if (options._.length !== 1) {
+  if (program.args.length !== 1) {
     console.error("Error: input file is required");
-    console.log(optimist.help());
+    console.log(program.help());
     return 0;
   }
 
   // Literate
-  var filename = options._[0];
+  var filename = program.args[0];
   var litContents;
   try {
-    litContents = literate(filename, { code: options.code });
+    litContents = literate(filename, { code: program.code });
   } catch (e) {
     console.error("Error: while literating -- " + e.message);
     return 1;
   }
 
   // Output
-  if (options.o) {
-    fs.writeFileSync(options.o, litContents);
+  if (program.output) {
+    fs.writeFileSync(program.output, litContents);
   } else {
     console.log(litContents);
   }
 }
 
-var ret = cli(process.argv.slice(2));
+var ret = cli(process.argv);
 process.exit(ret);
